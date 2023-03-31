4de0dd426363f5a5e07bd180e4423b985631da16
Mattia A. Fritz <mattia.fritz@unige.ch>
2023-03-31 14:37:22 +0200

    Ordinal regression almost done on data and results

--- fondements-statistiques/scripts-illustration/06_ordinal_glm.R
+++ fondements-statistiques/scripts-illustration/06_ordinal_glm.R
@@ -4,6 +4,7 @@ library(papaja)
 library(tidyverse)
 library(report)
 library(broom)
+library(emmeans)
 
 theme_set(theme_apa(box = TRUE))
 
@@ -12,7 +13,7 @@ n_points <- 200
 
 # Create a dataset with a clear effect of the predictor x
 x <- sample(c("A", "B"), n_points, replace = TRUE)
-w <- rnorm(n_points, mean = 30, sd = 5)
+w <- runif(n_points, min = 10, max = 30)
 latent_y <- ifelse(x == "A", rnorm(n_points, mean = 0, sd = 1), rnorm(n_points, mean = 1, sd = 1)) + w / 10
 outcome <- cut(latent_y, breaks = quantile(latent_y, probs = seq(0, 1, length.out = 6)), include.lowest = TRUE, labels = 1:5, ordered_result = TRUE)
 
@@ -20,17 +21,16 @@ data_glm <- data.frame(x, w, outcome)
 
 # Fit the ordered logistic regression model
 model_glm <- polr(outcome ~ x + w, data = data_glm, Hess = TRUE)
-summary(model_glm)
 
 # Plot the predicted probabilities
-plot_model(model_glm, type = "pred", terms = c("x"), )
+graph_glm <- plot_model(model_glm, type = "pred", terms = c("x", "w"))
 
 # Get the summary of the model
-model_summary <- summary(model_glm)
+model_glm_summary <- summary(model_glm)
 
 # Extract the coefficients and the standard errors
-coefs <- model_summary$coefficients[, "Value"]
-std_errors <- model_summary$coefficients[, "Std. Error"]
+coefs <- model_glm_summary$coefficients[, "Value"]
+std_errors <- model_glm_summary$coefficients[, "Std. Error"]
 
 # Calculate the z-values (t-values)
 z_values <- coefs / std_errors
@@ -38,16 +38,29 @@ z_values <- coefs / std_errors
 # Calculate the p-values
 p_values <- 2 * (1 - pnorm(abs(z_values)))
 
+format_pvalue <- function(p) {
+  if (p < 0.001) {
+    return("<0.001")
+  } else {
+    return(sprintf("%.3f", p))
+  }
+}
+
 # Combine the coefficients, standard errors, z-values, and p-values into a data frame
-results <- data.frame(
+results_glm <- data.frame(
   Estimate = coefs,
   `Std. Error` = std_errors,
   `z-value` = z_values,
   `p-value` = p_values
 )
 
-# Print the results
-print(results, digits = 3)
+results_glm$p.value = sapply(results_glm$p.value, format_pvalue)
+
+# Print the results_glm
+print(results_glm, digits = 3)
 
 equation_glm <- equatiomatic::extract_eq(model_glm, intercept = "beta")
 equation_glm_coeff <- equatiomatic::extract_eq(model_glm, use_coefs = TRUE, intercept = "beta")
+
+model_glm_contrast <- emmeans(model_glm, ~x | w) |> pairs()
+
